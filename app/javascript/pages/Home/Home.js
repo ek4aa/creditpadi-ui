f1feb8760495883d19f8645ac96107222fd60acb
ek4aa <eastking4aa@gmail.com>
2022-12-14 11:09:09 +0800

    Add: Structured React frontend skeleton

---
+++ app/javascript/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "../../components";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <h1>Home page!!!</h1>
+    </>
+  );
+}
+
+export { Home };
