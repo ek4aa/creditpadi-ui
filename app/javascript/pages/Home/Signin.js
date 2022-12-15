70445865028fbafad62886bc4f1ca09b1ba31d21
ek4aa <eastking4aa@gmail.com>
2022-12-15 12:47:13 +0800

    Update: completed auth pages

--- app/javascript/pages/Home/Signin.js
+++ app/javascript/pages/Home/Signin.js
@@ -10,48 +10,57 @@ function Signin() {
 
   return (
     <div className="authframe">
-      <div className="signin-content grid grid-cols-1 md:grid-cols-2 md:h-full">
-        <div className="left-panel hidden md:block bg-blue position-relative"></div>
-        <div className="right-panel">
-          <div className="row g-3 justify-content-end mx-5 my-auto">
-            <div class="col-auto">
-              <label class="col-form-label">I don’t have an account. </label>
+      <div className="signup-content row g-0">
+        <div className="left-panel d-none d-md-block col-6 col-md-6"></div>
+        <div className="right-panel col-12 col-md-6">
+          <div className="container">
+            <div className="row g-3 justify-content-end mx-1 my-auto">
+              <div className="col-auto">
+                <label className="col-form-label">
+                  I don't have an account{" "}
+                </label>
+              </div>
+              <div className="col-auto">
+                <button
+                  type="button"
+                  className="btn btn-default rounded-pill px-4"
+                  onClick={() => handleClick("/signup")}
+                >
+                  Sign Up
+                </button>
+              </div>
             </div>
-            <div class="col-auto">
-              <button
-                type="button"
-                className="btn btn-default rounded-pill px-4"
-                onClick={() => handleClick("/signup")}
-              >
-                Sign Up
-              </button>
+            <div className="container p-5 m-auto">
+              <form>
+                <div className="m-5 pt-5">
+                  <h1 className="page-title fw-bold text-center">Welcome Back</h1>
+                  <h3>Sign In your Creditpadi account</h3>
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    id="email"
+                    placeholder="Email address"
+                  />
+                </div>
+                <div className="mb-5">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    id="password"
+                    placeholder="Password"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-default btn-lg rounded-pill px-4 w-100"
+                >
+                  Sign In
+                </button>
+              </form>
             </div>
           </div>
-          <form className="h-full">
-            <div class="mb-3">
-              <h2 className="page-title">Welcome Back</h2>
-              <h5 className="">Sign In your Creditpadi account</h5>
-            </div>
-            <div class="mb-3">
-              <input
-                type="email"
-                class="form-control"
-                id="email"
-                placeholder="Email address"
-              />
-            </div>
-            <div class="mb-3">
-              <input
-                type="password"
-                class="form-control"
-                id="password"
-                placeholder="Password"
-              />
-            </div>
-            <button type="submit" className="btn btn-default rounded-pill px-4">
-              Sign In
-            </button>
-          </form>
         </div>
       </div>
     </div>
--- app/javascript/pages/Home/Signup.js
+++ app/javascript/pages/Home/Signup.js
@@ -10,93 +10,100 @@ function Signup() {
 
   return (
     <div className="authframe">
-      <div className="signup-content grid grid-cols-1 md:grid-cols-2 md:h-full">
-        <div className="left-panel hidden md:block bg-blue position-relative"></div>
-        <div className="right-panel">
-          <div className="row g-3 justify-content-end mx-5 my-auto">
-            <div class="col-auto">
-              <label class="col-form-label">Already a member?</label>
+      <div className="signup-content row g-0">
+        <div className="left-panel d-none d-md-block col-6 col-md-6"></div>
+        <div className="right-panel col-12 col-md-6">
+          <div className="container">
+            <div className="row g-3 justify-content-end mx-1 my-auto">
+              <div className="col-auto">
+                <label className="col-form-label">Already a member?</label>
+              </div>
+              <div className="col-auto">
+                <button
+                  type="button"
+                  className="btn btn-default rounded-pill px-4"
+                  onClick={() => handleClick("/signin")}
+                >
+                  Sign In
+                </button>
+              </div>
             </div>
-            <div class="col-auto">
-              <button
-                type="button"
-                className="btn btn-default rounded-pill px-4"
-                onClick={() => handleClick("/signin")}
-              >
-                Sign In
-              </button>
+            <div className="container p-5 m-auto">
+              <form>
+                <div className="m-5">
+                  <h3 className="page-title text-center">
+                    Sign Up for an account
+                  </h3>
+                </div>
+                <div className="d-grid gap-4 social-sign-btn-group">
+                  <button type="button" className="btn btn-lg btn-outline-default">
+                    <img
+                      className="img-fluid float-start"
+                      src="./assets/auth/google.png"
+                      alt="GoogleLogo"
+                      width={35}
+                    />
+                    <span className="m-auto">Sign Up with Google</span>
+                  </button>
+                  <button type="button" className="btn btn-lg btn-outline-default">
+                    <img
+                      className="img-fluid float-start"
+                      src="./assets/auth/phone.png"
+                      alt="PhoneLogo"
+                      width={30}
+                    />
+                    Sign Up with Phone Number
+                  </button>
+                </div>
+                <div className="hr-area my-5">
+                  <hr />
+                  <span className="hr-area-context">
+                    Or create an account with your email
+                  </span>
+                </div>
+                <div className="row g-3 mb-3">
+                  <div className="col-5">
+                    <input
+                      type="text"
+                      className="form-control form-control-lg"
+                      id="firstname"
+                      placeholder="First Name"
+                    />
+                  </div>
+                  <div className="col-7">
+                    <input
+                      type="text"
+                      className="form-control form-control-lg"
+                      id="lastname"
+                      placeholder="Last Name"
+                    />
+                  </div>
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    id="email"
+                    placeholder="Email address"
+                  />
+                </div>
+                <div className="mb-5">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    id="password"
+                    placeholder="Password"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-default btn-lg rounded-pill px-4 w-100"
+                >
+                  Sign Up
+                </button>
+              </form>
             </div>
           </div>
-          <form className="h-full">
-            <div class="mb-3">
-              <h3 className="page-title">Sign Up for an account</h3>
-            </div>
-            <div class="social-sign-btn-group mb-3">
-              <button type="button" class="btn btn-outline-default">
-                <img
-                  className="img-fluid me-3"
-                  src="./assets/auth/google.png"
-                  alt="GoogleLogo"
-                  width={35}
-                />
-                Sign Up with Google
-              </button>
-            </div>
-            <div class="social-sign-btn-group mb-3">
-              <button type="button" class="btn btn-outline-default">
-                <img
-                  className="img-fluid me-3"
-                  src="./assets/auth/phone.png"
-                  alt="PhoneLogo"
-                  width={35}
-                />
-                Sign Up with Phone Number
-              </button>
-            </div>
-            <div className="hr-area my-5">
-              <hr />
-              <p className="hr-area-context">
-                Or create an account with your email
-              </p>
-            </div>
-            <div class="row g-3 mb-3">
-              <div class="col-auto">
-                <input
-                  type="text"
-                  class="form-control"
-                  id="firstname"
-                  placeholder="First Name"
-                />
-              </div>
-              <div class="col-auto">
-                <input
-                  type="text"
-                  class="form-control"
-                  id="lastname"
-                  placeholder="Last Name"
-                />
-              </div>
-            </div>
-            <div class="mb-3">
-              <input
-                type="email"
-                class="form-control"
-                id="email"
-                placeholder="Email address"
-              />
-            </div>
-            <div class="mb-3">
-              <input
-                type="password"
-                class="form-control"
-                id="password"
-                placeholder="Password"
-              />
-            </div>
-            <button type="submit" className="btn btn-default rounded-pill px-4">
-              Sign Up
-            </button>
-          </form>
         </div>
       </div>
     </div>
